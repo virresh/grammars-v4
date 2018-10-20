d12b2db4d8ba60992388f58b0121040524ed498a
Viresh Gupta <viresh16118@iiitd.ac.in>
2018-10-20 14:18:01 +0530

    File without EOF to check failure

---
+++ python3-py/examples/simple_end_of_line.py
@@ -0,0 +1,7 @@
+# Test grammar file Python v3.3.7
+# Python test set -- part 1, grammar.
+# This just tests whether the parser accepts them all.
+
+def small_test():
+    """This file does not end in a newline"""
+    error = True
